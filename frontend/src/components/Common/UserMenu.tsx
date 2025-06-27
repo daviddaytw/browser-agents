3032fcbe4412420df6f1ce14e51280d5a88ea227
David Day <dj6082013@gmail.com>
2025-06-26 19:39:14 -0700

    Move api path.

--- frontend/src/components/Common/UserMenu.tsx
+++ frontend/src/components/Common/UserMenu.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react"
 import { Link } from "@tanstack/react-router"
 import { FaUserAstronaut } from "react-icons/fa"
-import { FiLogOut, FiUser } from "react-icons/fi"
+import { FiBook, FiLogOut, FiUser } from "react-icons/fi"
 
 import useAuth from "@/hooks/useAuth"
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "../ui/menu"
@@ -38,6 +38,17 @@ const UserMenu = () => {
                 <Box flex="1">My Profile</Box>
               </MenuItem>
             </Link>
+
+            <MenuItem
+              value="api-docs"
+              gap={2}
+              py={2}
+              onClick={() => window.open('/api/docs', '_blank')}
+              style={{ cursor: "pointer" }}
+            >
+              <FiBook fontSize="18px" />
+              <Box flex="1">API Documentation</Box>
+            </MenuItem>
 
             <MenuItem
               value="logout"
